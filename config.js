917bef7a3ac784032d69b9aa41b97f5820d080c3
hyuk6188 <ph1809@fursys.com>
2026-06-09 15:10:16 +0900

    Connect dashboard to Google Sheets sync

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  googleSheetsWebAppUrl: '',
+  googleSheetsWebAppUrl: 'https://script.google.com/macros/s/AKfycbxz1PXXxsYH6WQuhVj4ykhc22x1K3VNlc9GjgDbySggikl2oHPbNtoiyu3GCuGDXPhHVg/exec',
   googleSheetUrl: 'https://docs.google.com/spreadsheets/d/1L2njUjenWShSnQPGfqAIlBKA1A_yhwg-mTvRlQSXAw4/edit',
   syncIntervalMs: 15000
 };
